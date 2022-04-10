5c59b14666454937c866ed1a7590df65d05547b0
WaleAkinlabi <97405681+WaleAkinlabi@users.noreply.github.com>
2022-04-10 14:45:28 +0100

    Update firstjs.js

--- firstjs.js
+++ firstjs.js
@@ -1,102 +1,102 @@
-// let your_email = prompt("what is your email");
-
-// 
-// else if(your_email.indexOf("@") === -1){
-//     console.log("Invalid email");
-// }
-
-// else if(your_email.indexOf(".com") === -1){
-//     console.log("This email is invalid");
-// }
-// else{if(your_email.length === 0){
-//     console.log("email cannot be empty");
-// }
-
-//     console.log('Your email is : ' + your_email);
-    // }
+let your_email = prompt("what is your email");
 
 
-// let password = "abdullah2020";
+else if(your_email.indexOf("@") === -1){
+    console.log("Invalid email");
+}
 
-// let userPassword = prompt("Enter Password");
+else if(your_email.indexOf(".com") === -1){
+    console.log("This email is invalid");
+}
+else{if(your_email.length === 0){
+    console.log("email cannot be empty");
+}
 
-// while(userPassword !== password){
-//     alert("Wrong Password, please try again");
-//     userPassword = prompt("Enter Password");
-
-// }
-
-// alert("Congratulations Sigin successful");
-
-
-// Function without Argument
-// function changeCase(){
-//     let name = "abdullah";
-//     console.log(name.toUpperCase())
-// }
-
-// // Function with Argument
-// function capitalizeText(personName){
-//     console.log(personName.toUpperCase());
-// }
+    console.log('Your email is : ' + your_email);
+    }
 
 
-// function add(a, b){
-//     let result = a + b;
-//     console.log(result);
-//     return result;
-// }
+let password = "abdullah2020";
 
-// function multiply(f, j){
-//     let result =  f * j;
-//     return result;
-// }
-// multiply(20,3)
+let userPassword = prompt("Enter Password");
 
-// let final = 50 + multiply(2, add(15, 3));
-// console.log(final)
+while(userPassword !== password){
+    alert("Wrong Password, please try again");
+    userPassword = prompt("Enter Password");
+
+}
+
+alert("Congratulations Sigin successful");
+
+
+Function without Argument
+function changeCase(){
+    let name = "abdullah";
+    console.log(name.toUpperCase())
+}
+
+// Function with Argument
+function capitalizeText(personName){
+    console.log(personName.toUpperCase());
+}
+
+
+function add(a, b){
+    let result = a + b;
+    console.log(result);
+    return result;
+}
+
+function multiply(f, j){
+    let result =  f * j;
+    return result;
+}
+multiply(20,3)
+
+let final = 50 + multiply(2, add(15, 3));
+console.log(final)
 
 
     
 
-// Function with Argument
-// function adjust(word){
-//     console.log(word.replace(/-/g,"_"));
-// }
-// adjust("Hello-World")
+Function with Argument
+function adjust(word){
+    console.log(word.replace(/-/g,"_"));
+}
+adjust("Hello-World")
 
-// let my_word = "abdulla-akinlabi-olatunji-yusuf-";
+let my_word = "abdulla-akinlabi-olatunji-yusuf-";
 
-// adjust(my_word);
+adjust(my_word);
 
 
-// let secretnumber = 70;
-// let guessnumber = prompt("guess the number");
+let secretnumber = 70;
+let guessnumber = prompt("guess the number");
 
-// while(guessnumber != secretnumber){
-//     if(guessnumber > 1 && guessnumber < 20){
-//         alert("Too low, You are like 50 numbers below");
-//         guessnumber = prompt("Guess the Number");
-//     }
-//     else if(guessnumber > 20 && guessnumber < 50){
-//         alert("Too low, You are like 20 numbers below");
-//         guessnumber = prompt("Guess the Number");
-//     }
-//     else if(guessnumber > 50 && guessnumber < 69){
-//         alert("Too low, You are so close, try again");
-//         guessnumber = prompt("Guess the Number");
-//     }
-//     else if(guessnumber > 71 && guessnumber < 90){
-//         alert("Too high, you are about 20 numbers above");
-//         guessnumber = prompt("Guess the Number");
-//     }
-//     else if(guessnumber > 90){
-//         alert("Too high");
-//         guessnumber = prompt("Guess the Number");
-//     }
-// }
+while(guessnumber != secretnumber){
+    if(guessnumber > 1 && guessnumber < 20){
+        alert("Too low, You are like 50 numbers below");
+        guessnumber = prompt("Guess the Number");
+    }
+    else if(guessnumber > 20 && guessnumber < 50){
+        alert("Too low, You are like 20 numbers below");
+        guessnumber = prompt("Guess the Number");
+    }
+    else if(guessnumber > 50 && guessnumber < 69){
+        alert("Too low, You are so close, try again");
+        guessnumber = prompt("Guess the Number");
+    }
+    else if(guessnumber > 71 && guessnumber < 90){
+        alert("Too high, you are about 20 numbers above");
+        guessnumber = prompt("Guess the Number");
+    }
+    else if(guessnumber > 90){
+        alert("Too high");
+        guessnumber = prompt("Guess the Number");
+    }
+}
 
-// alert('You guessed right')
+alert('You guessed right')
 
 
 
@@ -116,32 +116,32 @@
 //Don't ask who is at the door anymore.
 
 
-// let activePerson = "director";
-// let personName = prompt("Who is at the door").toLowerCase();
+let activePerson = "director";
+let personName = prompt("Who is at the door").toLowerCase();
 
-// while(personName != activePerson.toLowerCase()){
-//     if(personName === "amina".toLowerCase()){
-//         alert("Girls are not allowed");
-//         personName = prompt("Who is at the door").toLowerCase();
-//     }
-//     else if(personName === "ibrahim".toLowerCase()){
-//         alert("Kids are not allowed to enter");
-//         personName = prompt("Who is at the door").toLowerCase();
-//     }
-//     else if(personName === "abdulsalam".toLowerCase()){
-//         alert("Kids are not allowed to enter");
-//         personName = prompt("Who is at the door").toLowerCase();
-//     }
-//     else if(personName === "muhammad".toLowerCase()){
-//         alert("You can enter but you need to behave yourself");
-//         personName = prompt("Who is at the door").toLowerCase();
-//     }
-//     else{
-//         alert("Who knows you".toLowerCase());
-//         personName = prompt("Who is at the door").toLowerCase();
-//     }
-// }
-// alert("welcome sir")
+while(personName != activePerson.toLowerCase()){
+    if(personName === "amina".toLowerCase()){
+        alert("Girls are not allowed");
+        personName = prompt("Who is at the door").toLowerCase();
+    }
+    else if(personName === "ibrahim".toLowerCase()){
+        alert("Kids are not allowed to enter");
+        personName = prompt("Who is at the door").toLowerCase();
+    }
+    else if(personName === "abdulsalam".toLowerCase()){
+        alert("Kids are not allowed to enter");
+        personName = prompt("Who is at the door").toLowerCase();
+    }
+    else if(personName === "muhammad".toLowerCase()){
+        alert("You can enter but you need to behave yourself");
+        personName = prompt("Who is at the door").toLowerCase();
+    }
+    else{
+        alert("Who knows you".toLowerCase());
+        personName = prompt("Who is at the door").toLowerCase();
+    }
+}
+ alert("welcome sir")
 
 // DATA STRUCTURES
 
